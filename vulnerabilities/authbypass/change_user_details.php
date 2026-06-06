e961ec5592e6279a7f29b496998b935b41456238
LeonAlvarez <itmestudiante2026@gmail.com>
2026-06-06 16:57:23 -0500

    Corrección SQL Injection con prepared statements

--- vulnerabilities/authbypass/change_user_details.php
+++ vulnerabilities/authbypass/change_user_details.php
@@ -44,9 +44,14 @@ try {
 	exit;
 }
 
-$query = "UPDATE users SET first_name = '" . $data->first_name . "', last_name = '" .  $data->surname . "' where user_id = " . $data->id . "";
-$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
-print json_encode (array ("result" => "ok"));
+$stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], 
+    "UPDATE users SET first_name = ?, last_name = ? WHERE user_id = ?"
+);
+
+mysqli_stmt_bind_param($stmt, "sss", $data->first_name, $data->surname, $data->id);
+
+mysqli_stmt_execute($stmt);
+
 exit;
 ?>
