0ed5b163c9f4a16c6ab738bdb6cdf40487bdabbc
LeonAlvarez <itmestudiante2026@gmail.com>
2026-06-06 17:54:34 -0500

    Corrección segura File Upload validación antes de subir

--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -1,19 +1,28 @@
 <?php
 
-if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+if (isset($_POST['Upload'])) {
 
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
-	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-	}
+    $target_path = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
+    $target_path .= basename($_FILES['uploaded']['name']);
+
+    $allowed_types = ['image/jpeg', 'image/png'];
+    $file_type = mime_content_type($_FILES['uploaded']['tmp_name']);
+
+    $allowed_extensions = ['jpg', 'jpeg', 'png'];
+    $file_extension = strtolower(pathinfo($target_path, PATHINFO_EXTENSION));
+
+    // ✅ Validar primero
+    if (in_array($file_type, $allowed_types) && in_array($file_extension, $allowed_extensions)) {
+
+        // ✅ Luego subir archivo
+        if (move_uploaded_file($_FILES['uploaded']['tmp_name'], $target_path)) {
+            $html .= "<pre>{$target_path} successfully uploaded!</pre>";
+        } else {
+            $html .= '<pre>Your file was not uploaded.</pre>';
+        }
+
+    } else {
+        $html .= '<pre>Archivo no permitido</pre>';
+    }
 }
 
-?>
