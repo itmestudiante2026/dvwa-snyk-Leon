8c4efe3f201e14b31f3b518a695b1af52137e486
LeonAlvarez <itmestudiante2026@gmail.com>
2026-06-06 18:19:06 -0500

    Corrección SQLi completa MySQL y SQLite con prepared statements

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,56 +1,62 @@
 <?php
 
 if( isset( $_REQUEST[ 'Submit' ] ) ) {
-	// Get input
-	$id = $_REQUEST[ 'id' ];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+    // Mejor usar GET específicamente
+    $id = $_GET['id'];
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
+    switch ($_DVWA['SQLI_DB']) {
 
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-			}
+        case MYSQL:
 
-			mysqli_close($GLOBALS["___mysqli_ston"]);
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
+            //  Prepared statement seguro
+            $stmt = mysqli_prepare(
+                $GLOBALS["___mysqli_ston"],
+                "SELECT first_name, last_name FROM users WHERE user_id = ?"
+            );
 
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
+            mysqli_stmt_bind_param($stmt, "s", $id);
+            mysqli_stmt_execute($stmt);
 
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
+            $result = mysqli_stmt_get_result($stmt);
 
-			if ($results) {
-				while ($row = $results->fetchArray()) {
-					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
+            while( $row = mysqli_fetch_assoc( $result ) ) {
+                $first = $row["first_name"];
+                $last  = $row["last_name"];
 
-					// Feedback for end user
-					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
-	} 
+                $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+            }
+
+            mysqli_close($GLOBALS["___mysqli_ston"]);
+            break;
+
+
+        case SQLITE:
+
+            global $sqlite_db_connection;
+
+            //  Prepared statement SQLITE
+            $stmt = $sqlite_db_connection->prepare(
+                "SELECT first_name, last_name FROM users WHERE user_id = :id"
+            );
+
+            $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+
+            $results = $stmt->execute();
+
+            if ($results) {
+                while ($row = $results->fetchArray()) {
+                    $first = $row["first_name"];
+                    $last  = $row["last_name"];
+
+                    $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+                }
+            } else {
+                echo "Error in fetch";
+            }
+
+            break;
+    }
 }
 
 ?>
